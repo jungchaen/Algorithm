30b8a51c447da1b7e960a26680a318c64e51cd46
jungchuen <50093044+jungchuen@users.noreply.github.com>
2022-08-07 23:52:23 +0900

    Create 1715.java

---
+++ BaekJoon/Greedy/1715.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < N; i++) {
+            int data = Integer.parseInt(br.readLine());
+            pq.add(data);
+        }
+        int data1 = 0;
+        int data2 = 0;
+        int sum = 0;
+        while (pq.size() != 1) {
+            data1 = pq.remove();
+            data2 = pq.remove();
+            sum += data1 + data2;
+            pq.add(data1 + data2);
+        }
+        System.out.println(sum);
+    }
+}
