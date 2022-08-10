7a196845cb955eceea3b3d8c64d7503b08d3e915
jungchuen <50093044+jungchuen@users.noreply.github.com>
2022-08-10 17:36:01 +0900

    Update 1715.java

--- BaekJoon/Greedy/1715.java
+++ BaekJoon/Greedy/1715.java
@@ -6,15 +6,17 @@ import java.util.PriorityQueue;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int N = Integer.parseInt(br.readLine()); // card size
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(); // 낮은 숫자가 우선순위인 큐
         for (int i = 0; i < N; i++) {
-            int data = Integer.parseInt(br.readLine());
-            pq.add(data);
+            pq.add(Integer.parseInt(br.readLine()));
         }
+
         int data1 = 0;
         int data2 = 0;
         int sum = 0;
+
         while (pq.size() != 1) {
             data1 = pq.remove();
             data2 = pq.remove();
