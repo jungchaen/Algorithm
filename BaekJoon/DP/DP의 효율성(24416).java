eecb6db96211f19c975486eb410ec75e6c37bf64
jungchaen <50093044+jungchaen@users.noreply.github.com>
2022-08-28 23:54:24 +0900

    Update DP의 효율성(24416).java

--- BaekJoon/DP/DP의 효율성(24416).java
+++ BaekJoon/DP/DP의 효율성(24416).java
@@ -17,14 +17,14 @@ public class Main {
     }
 
     public static int fibonacci(int n) { // dp를 이용한 피보나치 수열
-        int[] dp = new int[n];
-        dp[0] = 1;
+        int[] dp = new int[n + 1];
         dp[1] = 1;
-        for (int i = 2; i < n; i++) {
+        dp[2] = 1;
+        for (int i = 3; i <= n; i++) {
             dpCnt++;
             dp[i] = dp[i - 1] + dp[i - 2];
         }
-        return dp[n - 1];
+        return dp[n];
     }
 
     public static void main(String[] args) throws IOException {
