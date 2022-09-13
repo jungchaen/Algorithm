ce89c94d4f7f45ee03f10bbc913a21378b2658fd
jungchaen <50093044+jungchaen@users.noreply.github.com>
2022-09-13 21:25:54 +0900

    Update 이친수 구하기(2193).java

--- BaekJoon/DP/이친수 구하기(2193).java
+++ BaekJoon/DP/이친수 구하기(2193).java
@@ -2,19 +2,19 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main{
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         int n=Integer.parseInt(br.readLine());
-        long d[][]=new long[n+1][2];
+        long[][] D=new long[n+1][2]; //1의 자리부터 시작하니깐 n+1
+        //앞에는 자리수이고 뒤에는 끝자리 수 0,1
+        D[1][1]=1;
+        D[1][0]=0;
 
-        d[1][1]=1;
-        d[1][0]=0;  //이친수는 0으로 시작하지 않는다.
-
-        for (int i=2;i<=n;i++) {
-            d[i][0]=d[i-1][1]+d[i-1][0];
-            d[i][1]=d[i-1][0];
+        for(int i=2;i<=n;i++){ //이친수의 자리수만큼 반복
+            D[i][0]=D[i-1][1]+D[i-1][0];
+            D[i][1]=D[i-1][0];
         }
-        System.out.println(d[n][0]+d[n][1]);
+        System.out.println(D[n][0]+D[n][1]);
     }
 }
