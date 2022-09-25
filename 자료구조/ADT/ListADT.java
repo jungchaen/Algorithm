61ac52772b8670f5f71cceafc5de59b3f2e86f07
jungchaen <50093044+jungchaen@users.noreply.github.com>
2022-09-25 16:14:12 +0900

    Update ListADT.java

--- 자료구조/ADT/ListADT.java
+++ 자료구조/ADT/ListADT.java
@@ -1,24 +1,24 @@
 public interface ListADT<E> {
     /*Add element x to List index i*/
-    public void add(int i, E x);
+    public void add(int index, E item);
 
     /*Add element x to the end of the list*/
-    public void append(E x);
+    public void append(E item);
 
     /*delete the index i-th element*/
-    public E remove(int i);
+    public E remove(int index);
 
     /*delete the first x in List*/
-    public boolean removeItem(E x);
+    public boolean removeItem(E item);
 
     /*return the i-th element*/
-    public E get(int i);
+    public E get(int index);
 
     /*Change the value of the i-th element to x*/
-    public void set(int i, E x);
+    public void set(int index, E item);
 
     /*What is the index of the element?*/
-    public int indexOf(E x);
+    public int indexOf(E item);
 
     /*total number of elements*/
     public int len();
